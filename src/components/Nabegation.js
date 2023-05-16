5f5c43e7c3c525a2b88d0e9a41499958e72f6749
santiago <santysjr956@gmail.com>
2023-05-16 01:25:45 -0300

    best code and auth complete

--- src/components/Nabegation.js
+++ src/components/Nabegation.js
@@ -5,6 +5,8 @@ import { logout } from "../slices/auth";
 import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
+import { useEffect } from "react";
+import Link from "next/link";
 
 function Nabegation() {
   const { push } = useRouter();
@@ -21,20 +23,39 @@ function Nabegation() {
   return (
     <Navbar bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="/">Store</Navbar.Brand>
+        <Link href="/">
+          <Navbar.Brand>Store</Navbar.Brand>
+        </Link>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           {isLoggedIn && (
             <>
-              <Navbar.Text> Signed in as: {user.user} </Navbar.Text>
-              <Navbar.Text onClick={headleDeslogarse}>Logout</Navbar.Text>
               {user.authorities.map((a) => {
                 return (
-                  <Navbar.Text key={a.authority}>
-                    {a.authority == "ADMIN" ? "admin" : "user"}
-                  </Navbar.Text>
+                  <Link
+                    key={a.authority}
+                    href={
+                      a.authority == "ADMIN"
+                        ? "/admindesboard"
+                        : "/userdesboard"
+                    }
+                  >
+                    <Navbar.Text>ir al desboard </Navbar.Text>
+                  </Link>
                 );
               })}
+              <Navbar.Text>
+                Signed in as
+                {user.authorities.map((a) => {
+                  return (
+                    <Navbar.Text key={a.authority}>
+                      {a.authority == "ADMIN" ? " admin" : " user"}
+                    </Navbar.Text>
+                  );
+                })}
+                : {user.user}{" "}
+              </Navbar.Text>
+              <Navbar.Text onClick={headleDeslogarse}>Logout</Navbar.Text>
             </>
           )}
         </Navbar.Collapse>
